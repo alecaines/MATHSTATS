1a7de94ca9cfc29e770c3e84d071cca666f692f2
alecaines <alecaines@gmail.com>
2021-03-22 11:04:02 -0400

    started

---
+++ hmwk/hmwk3/p1.r
@@ -0,0 +1,8 @@
+data <- read.csv("ex11-17.csv")
+
+colnames(data)
+table(data$Sand.Addition...., data$Carbon.Fiber.Addition...., data$Casting.Hardness, data$Wet.Mold.Strength)
+
+#1b
+res.aov2 <- aov(Casting.Hardness ~ Sand.Addition.... + Carbon.Fiber.Addition.... + Wet.Mold.Strength, data=data)
+summary(res.aov2)
