aaaf0176b923aa0ce36a4d0d3d0c84fd61f4f94d
alecaines <alecaines@gmail.com>
2021-04-14 16:55:34 -0400

    i need to find a new place to work

--- final/p2.r
+++ final/p2.r
@@ -1,31 +1,44 @@
 data <- read.csv('Final4.csv')
 
 mod2 <- function(s) if(s=="Yes") 1 else 0
+death_rate <- function(L) sum(L[L == 1])/length(L)
 
 white <- data[data$Race == "White, Non-Hispanic",]
 white_deaths <- as.numeric(lapply(white$Death_yn, mod2))
-shapiro.test(white_deaths)
+wdr <- death_rate(white_deaths)
+#shapiro.test(white_deaths)
 
 asian <- data[data$Race == "Asian, Non-Hispanic",]
 asian_deaths <- as.numeric(lapply(asian$Death_yn, mod2))
-shapiro.test(asian_deaths)
+adr <- death_rate(asian_deaths)
+#shapiro.test(asian_deaths)
 
 hispanic <- data[data$Race == "Hispanic/Latino",]
 hispanic_deaths <- as.numeric(lapply(hispanic$Death_yn, mod2))
-shapiro.test(hispanic_deaths)
+
+
+#shapiro.test(hispanic_deaths)
 
 black <- data[data$Race == "Black, Non-Hispanic",]
 black_deaths <- as.numeric(lapply(black$Death_yn, mod2))
-shapiro.test(black_deaths)
+#shapiro.test(black_deaths)
 
 multiple <- data[data$Race == "Multiple/Other, Non-Hispanic",]
 multiple_death <- as.numeric(lapply(multiple$Death_yn, mod2))
-shapiro.test(multiple_death)
+#shapiro.test(multiple_death)
 
 amindian <- data[data$Race == "American Indian/Alaska Native, Non-Hispanic",]
 amindian_death <- as.numeric(lapply(amindian$Death_yn, mod2))
-shapiro.test(amindian_death)
+#shapiro.test(amindian_death)
 
 nathawaiian <- data[data$Race == "Native Hawaiian/Other Pacific Islander, Non-Hispanic",]
-nathawaiian_death <- as.numeric(lapply(nathawaiian$Death_yn, mod2))
-shapiro.test(nathawaiian_death)
+#nathawaiian_death <- as.numeric(lapply(nathawaiian$Death_yn, mod2))
+#shapiro.test(nathawaiian_death)
+
+
+eths <- c(unique(data["Race"]))
+df <- data.frame(matrix(ncol = length(eths), nrow = length(data["Race"])))
+colnames(df) <- eths
+for(e in eths){
+  df$e <- data[data$Race == e,]
+}
