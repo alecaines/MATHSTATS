fde723af3ec6f3eb865697c697a19462dcb16129
alecaines <alecaines@gmail.com>
2021-04-14 23:02:50 -0400

    working up

--- final/p2.r
+++ final/p2.r
@@ -15,30 +15,49 @@ adr <- death_rate(asian_deaths)
 
 hispanic <- data[data$Race == "Hispanic/Latino",]
 hispanic_deaths <- as.numeric(lapply(hispanic$Death_yn, mod2))
-
+hdr <- death_rate(hispanic_deaths)
 
 #shapiro.test(hispanic_deaths)
 
 black <- data[data$Race == "Black, Non-Hispanic",]
 black_deaths <- as.numeric(lapply(black$Death_yn, mod2))
+bdr <- death_rate(black_deaths)
 #shapiro.test(black_deaths)
 
 multiple <- data[data$Race == "Multiple/Other, Non-Hispanic",]
-multiple_death <- as.numeric(lapply(multiple$Death_yn, mod2))
+multiple_deaths <- as.numeric(lapply(multiple$Death_yn, mod2))
+mdr <- death_rate(multiple_deaths)
 #shapiro.test(multiple_death)
 
 amindian <- data[data$Race == "American Indian/Alaska Native, Non-Hispanic",]
 amindian_death <- as.numeric(lapply(amindian$Death_yn, mod2))
+amdr <- death_rate(amindian_death)
 #shapiro.test(amindian_death)
 
 nathawaiian <- data[data$Race == "Native Hawaiian/Other Pacific Islander, Non-Hispanic",]
 #nathawaiian_death <- as.numeric(lapply(nathawaiian$Death_yn, mod2))
 #shapiro.test(nathawaiian_death)
 
+adr
+amdr
+bdr
+hdr
+mdr
+wdr
+print("there is a higher death rate among american indians than any other population")
 
-eths <- c(unique(data["Race"]))
-df <- data.frame(matrix(ncol = length(eths), nrow = length(data["Race"])))
-colnames(df) <- eths
-for(e in eths){
-  df$e <- data[data$Race == e,]
-}
+
+#eths <- c(unique(data["Race"]))
+#df <- data.frame(matrix(ncol = length(eths), nrow = length(data["Race"])))
+#colnames(df) <- eths
+#for(e in eths){
+#  df$e <- data[data$Race == e,]
+#}
+
+death_rates <- data[, c("Race", "Death_yn")]
+death_rates$Death_yn <- as.numeric(lapply(death_rates$Death_yn, mod2))
+head(death_rates)
+shapiro.test(death_rates$Death_yn)
+print("passes shapiro wilks")
+
+leveneTest(Death_yn ~ Race, data=death_rates)
